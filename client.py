676227d8e619bd132a26ecdef019939347e36d04
Stanley <stanley.yang.sy@gmail.com>
2013-11-02 03:53:51 -0400

    added basic prediction algorithm

--- client.py
+++ client.py
@@ -29,6 +29,7 @@ pNA = 0
 hAP = []
 hEU = []
 hNA = []
+highTurn = 0
 
 class AutoVivification(dict):
     """Implementation of perl's autovivification feature."""
@@ -151,11 +152,6 @@ def addHistory(region, num):
     else:
         hNA = temp
 
-"""
-    if (len(temp) < 3):
-        return 0
-"""
-
 def calcChange(region):
     global pAP
     global pEU
@@ -176,6 +172,85 @@ def calcChange(region):
 
     if(len(temp) < 5):
         return 0
+
+    dx = temp[4] - temp[3]
+    dx2 = dx - temp[3] - temp[2]
+
+    p = calcNext(temp, dx, dx2)
+
+    if(region == 'AP'):
+        pAP = p
+        print "PREDICTED FOR ASIA IS: " + str(p)
+    elif(region == 'EU'):
+        pEU = p
+        print "PREDICTED FOR EUROPE IS: " + str(p)
+    else:
+        pNA = p
+        print "PREDICTED FOR AMERICA IS: " + str(p)
+
+def calcNext(history, dx, dx2):
+    current = history[4]
+    trend = findTrend(history)
+    ans = 0
+    if(dx * dx2 < 0 and abs(dx) < 20):
+        if(dx > 0):
+            ans = current + int(1.125 * dx)
+        else:
+            ans = current - int(1.125 * dx)
+
+    if(trend == 1 or trend == -1):
+        ans = current
+
+    if(trend == 2 or trend == -2):
+        if(trend > 0):
+            if(dx > 0):
+                ans = current + int(1.125 * dx)
+            else:
+                ans = current - int(.5 * dx)
+        else:
+            if (dx < 0):
+                ans = current - int(1.125 * dx)
+            else:
+                ans = current + int(.5 * dx)
+
+    if(trend == 3 or trend == -3):
+        if(trend > 0):
+            if(dx > 0):
+                ans = current + int(1.25 * dx)
+            else:
+                ans = current - int(.25 * dx)
+        else:
+            if(dx < 0):
+                ans = current - int(1.25 * dx)
+            else:
+                ans = current + int(.25 * dx)
+
+    if(trend == 4 or trend == -4):
+        if(trend > 0):
+            if(dx > 0):
+                ans = current + int(1.5 * dx)
+            else:
+                ans = current - (.125 * dx)
+        else:
+            if (dx < 0 ):
+                ans = current - int(1.5 * dx)
+            else:
+                ans = current + int(.125 * dx)
+
+    return ans
+
+def findTrend(a):
+    i = 0
+    if(a[4] > a[3]):
+        i = 1
+        while(i < 4 and a[len(a) - i - 1] > a[len(a)- i - 2]):
+            i=i+1
+    else:
+        i = -1
+        while(i > -5 and a[len(a) - 1 + i] < a[len(a) - 2 + i]):
+            i = i-1
+    
+    return i
 
     #lagrange save for maybe later use
 #    p = (temp[0] * 8) + (temp[1] * -15) + (temp[2] * 16) + (temp[3] * -6) + (temp[4] * 2)
@@ -203,29 +278,48 @@ def init():
 
 
 def main():
+    global highTurn
+    difAP = 0
+    difEU = 0
+    difNA = 0
     init()
     r = nextTurn()
 
     while(1):
         payout = r.json()
+        if(highTurn > getTurnNo(payout)):
+            print "CURRENT TURN IS: " + str(getTurnNo(payout))
+            break
         clearCR()
-
+        highTurn = getTurnNo(payout)
         turn = getTurnNo(payout)
-
         print ""
         print "CURRENT TURN IS: " + str(turn)
-
+        print getTransactionTime(payout)
         printWebTransactions(payout)
+        print ""
+        
+        if(turn > 10):
+            if(difAP < abs(hAP[4]-pAP)):
+                difAP = abs(hAP[4] - pAP)
+            if(difEU < abs(hEU[4] - pEU)):
+                difEU = abs(hEU[4]-pEU)
+            if(difNA < abs(hNA[4] - pNA)):
+                difNA = abs(hNA[4] - pNA)
+            print "Highest mistake ASIA: " + str(difAP)
+            print "Highest mistake EUROPE: " + str(difEU)
+            print "Highest mistake AMERICA: " + str(difNA)
+
         print ""
 #        print "HISTORY OF ASIA: " + str(hAP)
 #        print "HISTORY OF EUROPE: " + str(hEU)
 #        print "HISTORY OF AMERICA: " + str(hNA)
-#        calcChange('AP')
-#        calcChange('EU')
-#        calcChange('NA')
-#        print ""
+        calcChange('AP')
+        calcChange('EU')
+        calcChange('NA')
+        print ""
 
         r = nextTurn()
-        raw_input("Press Enter to continue...")
+        #raw_input("Press Enter to continue...")
 
 main()
