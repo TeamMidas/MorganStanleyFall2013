4a7b32df5cda41b2539e1fb0aedc8ed3b18e9b21
Stanley <stanley.yang.sy@gmail.com>
2013-11-02 11:14:36 -0400

    removed old CR stuff

--- client.py
+++ client.py
@@ -50,17 +50,6 @@ upID = 1
 
 fig = plt.figure()
 
-class AutoVivification(dict):
-    """Implementation of perl's autovivification feature."""
-    def __getitem__(self, item):
-        try:
-            return dict.__getitem__(self, item)
-        except KeyError:
-            value = self[item] = type(self)()
-            return value
-
-#CR = AutoVivification()
-
 #JSON parsing functions
 def getCostIncured(payout):
     return payout['ServerState']['CostIncured']
